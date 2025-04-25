f8a7d83ca0f18e0f5a2cea92a93fe2683b7e23cc
Giorgos Mathioudakis <mathioudakes@gmail.com>
2025-04-25 15:12:21 +0300

    Initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function TodoInput(props){
+
+    const { handleAddTodos , todoValue , setTodoValue } = props
+
+
+    return(
+        <header>
+            <input value={todoValue} onChange={(e)=>{
+                setTodoValue(e.target.value)
+            }} placehoder="Enter todo..." ></input>
+            <button onClick={()=>{
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }} >Add</button>
+        </header>
+    )
+}
